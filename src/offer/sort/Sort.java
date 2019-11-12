e0961ee29aecad90d0f2ba98fcaf8ee3d9c554bf
zhuwenwen0 <18895675919@163.com>
2019-11-12 23:02:37 +0800

    归并排序

--- src/offer/sort/Sort.java
+++ src/offer/sort/Sort.java
@@ -29,6 +29,52 @@ public class Sort {
         quickSort(a, last + 1, high);
     }
 
+
+    /**
+     * 归并排序
+     *
+     * @param a
+     * @param low
+     * @param high
+     * @return
+     */
+    public int[] mergeSort(int[] a, int low, int high) {
+        int mid = (low + high) / 2;
+        if (low < high) {
+            mergeSort(a, low, mid);
+            mergeSort(a, mid + 1, high);
+            merge(a, low, mid, high);
+        }
+        return a;
+    }
+
+    public void merge(int[] a, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+        // 把较小的数先移到新数组中
+        while (i <= mid && j <= high) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
+                temp[k++] = a[j++];
+            }
+        }
+        // 把左边剩余的数移入数组
+        while (i <= mid) {
+            temp[k++] = a[i++];
+        }
+        // 把右边边剩余的数移入数组
+        while (j <= high) {
+            temp[k++] = a[j++];
+        }
+        // 把新数组中的数覆盖nums数组
+        for (int x = 0; x < temp.length; x++) {
+            a[x + low] = temp[x];
+        }
+    }
+
     /**
      * 二分查找
      *
@@ -57,10 +103,14 @@ public class Sort {
     public static void main(String[] args) {
         Sort sort = new Sort();
         int[] a = {3, 3, 9, 6, 8, 5, 7, 10, 17, 12, 15};
-        sort.quickSort(a, 0, a.length - 1);
+        //sort.quickSort(a, 0, a.length - 1);
 //        for (int i = 0; i <a.length ; i++) {
 //            System.out.println(a[i]);
 //        }
-        System.out.println(sort.binarySearch(a, 0, a.length - 1, 11));
+        //System.out.println(sort.binarySearch(a, 0, a.length - 1, 11));
+        int[] ints = sort.mergeSort(a, 0, a.length - 1);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
     }
 }
