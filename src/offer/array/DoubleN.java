e7a9670322cbc44f759687a89d9c1dc345ed88b8
zhuwenwen0 <18895675919@163.com>
2019-11-12 21:41:19 +0800

    提交数组相关操作

---
+++ src/offer/array/DoubleN.java
@@ -0,0 +1,88 @@
+package offer.array;
+
+import java.util.Stack;
+
+/**
+ * 求一个double的n次方
+ *
+ * @author zhuwenwen
+ * @date 2019/11/11 21:40
+ **/
+public class DoubleN {
+
+    /**
+     * 求一个浮点型base的整数次方
+     *
+     * @param base     base
+     * @param exponent exponent
+     * @return
+     */
+    public double Power(double base, int exponent) {
+        if (base == 0 && exponent == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (base == 0) {
+            return 0;
+        }
+        if (exponent == 0) {
+            return 1;
+        }
+        double result = 1;
+        if (exponent > 0) {
+            for (int i = 0; i < exponent; i++) {
+                result = result * base;
+            }
+            return result;
+        }
+        for (int i = 0; i < -exponent; i++) {
+            result = result * base;
+        }
+        return 1 / result;
+    }
+
+    /**
+     * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，
+     * 所有的偶数位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+     * 思路：可以使用两个栈就可以轻易实现，不过遍历n,也可以使用一个栈，但是需要遍历两次
+     *
+     * @param array array
+     */
+    public void reOrderArray(int [] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+        //奇数栈
+        Stack<Integer> oddNumber = new Stack<>();
+        //偶数栈
+        Stack<Integer> evenNumbers = new Stack<>();
+        for (int i = array.length -1; i>= 0; i--) {
+            if (array[i] % 2 == 0) {
+                evenNumbers.push(array[i]);
+            } else {
+                oddNumber.push(array[i]);
+            }
+        }
+        int d = 0;
+        while (!oddNumber.empty()) {
+            array[d] = oddNumber.pop();
+            d++;
+        }
+        while (!evenNumbers.empty()) {
+            array[d] = evenNumbers.pop();
+            d++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DoubleN doubleN = new DoubleN();
+//        double power = doubleN.Power(-2, -3);
+//        System.out.println(power);
+        int[] array = {1, 4, 2, 3, 8, 6, 5, 7};
+        doubleN.reOrderArray(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+}
--- src/offer/matrix/PrintMatrix.java
+++ src/offer/matrix/PrintMatrix.java
@@ -1,5 +1,7 @@
 package offer.matrix;
 
+import java.util.ArrayList;
+
 /**
  * 打印顺时针矩阵
  *
@@ -10,32 +12,49 @@ public class PrintMatrix {
 
 
     /**
-     * 打印顺时针矩阵，比如给定一个矩阵4x4矩阵{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}
+     * s形打印矩阵，比如给定一个矩阵4x4矩阵{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}
      * 打印出{1,2,3,4,8,7,6,5,9,10,11,12,16,15,14,13}
      *
-     * @param array 矩阵
+     * @param matrix 矩阵
      */
-    public void print(int[][] array) {
-        if (array == null || array.length < 0 || array[0].length < 0) {
-            return;
+    public ArrayList<Integer> print(int[][] matrix) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        if (matrix == null || matrix.length < 0 || matrix[0].length < 0) {
+            return arrayList;
         }
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             //如果行数是偶数的话,代表从头开始打印,否则,从尾部开始打印
             if (i % 2 == 0) {
-                for (int j = 0; j < array[i].length; j++) {
-                    System.out.println(array[i][j]);
+                for (int j = 0; j < matrix[i].length; j++) {
+                    arrayList.add(matrix[i][j]);
                 }
             } else {
-                for (int j = array[i].length - 1; j >= 0; j--) {
-                    System.out.println(array[i][j]);
+                for (int j = matrix[i].length - 1; j >= 0; j--) {
+                    arrayList.add(matrix[i][j]);
                 }
             }
         }
+        return arrayList;
+    }
+
+    /**
+     * 顺时针打印矩阵 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，例如，如果输入如下4 X 4矩阵：
+     * 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+     *
+     * @param matrix matrix
+     * @return
+     */
+    public ArrayList<Integer> printMatrix(int [][] matrix) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        return arrayList;
     }
 
     public static void main(String[] args) {
         PrintMatrix printMatrix = new PrintMatrix();
         int[][] array = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        printMatrix.print(array);
+        ArrayList<Integer> print = printMatrix.print(array);
+        print.forEach(c -> {
+            System.out.println(c);
+        });
     }
 }
