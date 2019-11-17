5ce62c2c35f902b18ef3c8f02df6c3ee5390322a
zhuwenwen0 <18895675919@163.com>
2019-11-17 23:48:20 +0800

    提交顺时针打印矩阵

--- src/offer/matrix/PrintMatrix.java
+++ src/offer/matrix/PrintMatrix.java
@@ -40,21 +40,57 @@ public class PrintMatrix {
     /**
      * 顺时针打印矩阵 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，例如，如果输入如下4 X 4矩阵：
      * 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+     * <p>
+     * 思路：遍历四个边为一个循环,
      *
      * @param matrix matrix
      * @return
      */
-    public ArrayList<Integer> printMatrix(int [][] matrix) {
+    public ArrayList<Integer> printMatrix(int[][] matrix) {
         ArrayList<Integer> arrayList = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return arrayList;
+        }
+        int r1 = 0, r2 = matrix.length - 1, c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            //遍历第一条边
+            for (int i = c1; i <= c2; i++) {
+                arrayList.add(matrix[r1][i]);
+            }
+            //遍历第二条边
+            for (int i = r1 + 1; i <= r2; i++) {
+                arrayList.add(matrix[i][c2]);
+            }
+            //如果有多行的话，代表需要遍历第三条边
+            if (r1 != r2) {
+                for (int i = c2 - 1; i >= c1; i--) {
+                    arrayList.add(matrix[r2][i]);
+                }
+            }
+            //如果有多列的话，代表需要遍历第四条边
+            if (c1 != c2) {
+                for (int i = r2 - 1; i > r1; i--) {
+                    arrayList.add(matrix[i][c1]);
+                }
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
         return arrayList;
     }
 
     public static void main(String[] args) {
         PrintMatrix printMatrix = new PrintMatrix();
         int[][] array = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        ArrayList<Integer> print = printMatrix.print(array);
-        print.forEach(c -> {
+        ArrayList<Integer> arrayList = printMatrix.printMatrix(array);
+        arrayList.forEach(c -> {
             System.out.println(c);
         });
+//        ArrayList<Integer> print = printMatrix.print(array);
+//        print.forEach(c -> {
+//            System.out.println(c);
+//        });
     }
 }
