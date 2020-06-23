afb731969fd6a2c1eff3aa97922e69ce4743303a
zhuwenwen <zhuwenwen@unilifemedia.cn>
2020-06-23 13:56:23 +0800

    使用栈实现矩阵中的路径

--- src/algorithm/matrix/Matrix.java
+++ src/algorithm/matrix/Matrix.java
@@ -1,6 +1,8 @@
 package algorithm.matrix;
 
 
+import java.util.Stack;
+
 /**
  * @author zhuwenwen
  * @date 2019/2/13 15:05
@@ -12,6 +14,9 @@ public class Matrix {
     private final static int[][] next = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
     private int rows;
     private int cols;
+
+
+    //-------------------------------------------------------------------------------------------------------------------------------
 
     /**
      * 第一种方式,递归的方法   题目：请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一个格子开始
@@ -67,6 +72,91 @@ public class Matrix {
         return false;
     }
 
+    //第二种解法,使用栈实现深度遍历----------------------------
+
+    public boolean hasPath2(char[] array, int rows, int cols, char[] str) {
+        if (rows == 0 || cols == 0) {
+            return false;
+        }
+        this.rows = rows;
+        this.cols = cols;
+        boolean[][] marked = new boolean[rows][cols];
+        char[][] matrix = buildMatrix(array);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                //如果第一个元素匹配的话，就通过栈判断其是否含有此路径
+                if (matrix[i][j] == str[0]) {
+                    //如果包含此路径的话,就直接返回true,否则返回false
+                    if (backtrackingWithStock(matrix, str, marked, i, j)) {
+                        return true;
+                    }
+                    continue;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 判断矩阵中的一个符合字符串首字符的节点是否包含字符串的路径
+     *
+     * @param matrix 矩阵
+     * @param str    字符串路径数组
+     * @param marked 标记矩阵
+     * @param r      起始地址的行
+     * @param c      起始地址的列
+     * @return 是否包含
+     */
+    private boolean backtrackingWithStock(char[][] matrix, char[] str,
+                                          boolean[][] marked, int r, int c) {
+        Stack<MatrixPosition> stack = new Stack<>();
+        //进行标记
+        marked[r][c] = true;
+        stack.push(new MatrixPosition(r, c));
+        while (!stack.isEmpty()) {
+            for (int i = stack.size(); i < str.length; i++) {
+                //判断起始点的的后一个位置是否包含对应位置的字符
+                boolean add = false;
+                loop1:
+                for (int j = 0; j < next.length; j++) {
+                    //弹出栈顶元素，但是元素不出栈
+                    MatrixPosition peek = stack.peek();
+                    int nextX = peek.getX() + next[j][0];
+                    int nextY = peek.getY() + next[j][1];
+                    //限制下一步的行进范围在矩阵中
+                    if (nextX >= matrix.length || nextY >= matrix[0].length || nextX < 0 || nextY < 0 || marked[nextX][nextY]) {
+                        continue;
+                    }
+                    //如果是符合字符串结果就进栈
+                    if (matrix[nextX][nextY] == str[i]) {
+                        stack.push(new MatrixPosition(nextX, nextY));
+                        //标记已访问
+                        marked[nextX][nextY] = true;
+                        add = true;
+                        //如果是字符串的最后一个字符找到的话，就返回true
+                        if (i == str.length - 1) {
+                            return true;
+                        }
+                        //退出循环
+                        break loop1;
+                    }
+                }
+
+                if (!add) {
+                    //如果进行了四步都没有包含字符串所需的字符的话,就回溯上一步
+                    if (stack.size() == 1) {
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
     private char[][] buildMatrix(char[] array) {
         char[][] matrix = new char[rows][cols];
         for (int r = 0, idx = 0; r < rows; r++) {
@@ -76,6 +166,8 @@ public class Matrix {
         }
         return matrix;
     }
+
+    //---------------------------------------------------------------------------------------------------------------------------
 
     /**
      * 机器人的运动范围,地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，
@@ -89,7 +181,7 @@ public class Matrix {
      * @return 机器人可以运动的最大范围
      */
     public int movingCount(int threshold, int rows, int cols) {
-        if (rows <=0 || cols <= 0 || threshold <= 0) {
+        if (rows <= 0 || cols <= 0 || threshold <= 0) {
             return 0;
         }
         int[][] matrix = new int[rows][cols];
@@ -131,10 +223,10 @@ public class Matrix {
      */
     private int countTheNum(int x) {
         int sum = 0;
-        do{
-            sum += x%10;
+        do {
+            sum += x % 10;
 
-        }while((x = x/10) > 0);
+        } while ((x = x / 10) > 0);
 
 
         return sum;
@@ -169,6 +261,12 @@ public class Matrix {
 
     public static void main(String[] args) {
         Matrix matrix = new Matrix();
-        System.out.println(matrix.movingCount(2, 2, 3));
+        char[] chars = {'a','b','c','e','s','f','c','s','a','d','e','e'};
+        char[] path = {'b','c','c','e','d'};
+        char[] path2 = {'a','b','c','d'};
+        boolean b = matrix.hasPath2(chars, 3, 4, path);
+        boolean c = matrix.hasPath2(chars, 3, 4, path2);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
