5eafdc43d42bdd50c2ecf5394179bc8b4b94c791
zhuwenwen0 <18895675919@163.com>
2020-07-04 15:08:51 +0800

    数组中有一个数字出现的次数超过数组长度的一半

--- src/algorithm/normal/Sum.java
+++ src/algorithm/normal/Sum.java
@@ -1,6 +1,8 @@
 package algorithm.normal;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author zhuwenwen
@@ -282,8 +284,34 @@ public class Sum {
         return arrayList;
     }
 
+    /**
+     * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+     * 例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。由于数字2在数组中出现了5次，
+     * 超过数组长度的一半，因此输出2。如果不存在则输出0。
+     *
+     * @param array
+     * @return
+     */
+    public int MoreThanHalfNum_Solution(int [] array) {
+        if(array == null || array.length <= 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< array.length ; i++) {
+            if(map.get(array[i]) != null) {
+                map.put(array[i], map.get(array[i]) + 1);
+            } else {
+                map.put(array[i], 1);
+            }
+        }
 
-
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > array.length / 2) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
         Sum sum = new Sum();
