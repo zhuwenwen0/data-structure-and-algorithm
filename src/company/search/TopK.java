0bca368302a080eba9679e9ff2c141cd967ba70b
zhuwenwen <zhuwenwen@unilifemedia.cn>
2020-10-30 13:59:21 +0800

    寻找top k问题

---
+++ src/company/search/TopK.java
@@ -0,0 +1,31 @@
+package company.search;
+
+/**
+ * @author zhuwenwen
+ * @date 13:56 30-10-2020
+ **/
+public class TopK {
+
+
+    /**
+     * 给定String类型的数组strArr，再给定整数k，请严格按照排名顺序打印 出次数前k名的字符串。
+     * [要求]
+     * 如果strArr长度为N，时间复杂度请达到O(N \log K)O(NlogK)
+     *
+     * 输出K行，每行有一个字符串和一个整数（字符串表示）。
+     * 你需要按照出现出现次数由大到小输出，若出现次数相同时字符串字典序较小的优先输出
+     */
+    /**
+     * return topK string
+     *
+     *
+     * @param strings string字符串一维数组 strings
+     * @param k int整型 the k
+     * @return string字符串二维数组
+     */
+    public String[][] topKstrings (String[] strings, int k) {
+        // write code here
+        //todo 寻找top k问题
+        return null;
+    }
+}
